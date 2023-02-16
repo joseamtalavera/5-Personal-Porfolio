e2f1e420b81a0225c278cbc9eb989a331c67b8fc
jose am talavera <115177988+joseamtalavera@users.noreply.github.com>
2023-02-16 07:38:25 +0200

    time in our site

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
+/*
+
 const timeContainer = document.getElementById("time-container");
-    const BIRTH_DAY = "12-09-2022";
-    const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
-    const intlNumberFormatter = new Intl.NumberFormat("en-US");
+const BIRTH_DAY = "12-09-2022";
+const BIRTH_DAY_DATE = new Date(BIRTH_DAY);
+const intlNumberFormatter = new Intl.NumberFormat("en-US");
+
+
+
 
     setInterval(() => {
       const now = new Date();
@@ -11,3 +16,23 @@ const timeContainer = document.getElementById("time-container");
 
       timeContainer.innerText = intlNumberFormatter.format(difference);
     }, 1000);
+*/
+    
+
+
+
+    // Get a reference to the time container span element
+const timeContainer = document.getElementById('time-container');
+
+// Set the start time
+const startTime = new Date();
+
+// Update the time every second
+setInterval(() => {
+  // Calculate the difference in seconds between the start time and now
+  const diffInSeconds = Math.round((new Date() - startTime) / 1000);
+
+  // Update the text of the time container span element
+  timeContainer.textContent = diffInSeconds;
+}, 1000);
+
